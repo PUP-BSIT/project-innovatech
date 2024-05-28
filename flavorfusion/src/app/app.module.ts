9bf39d62b06e2028110655921313081db5a1de0d
ChrystineNoelle <asebuquechrystine@gmail.com>
2024-05-28 19:59:12 +0800

    add components for home, community, profile, and search

--- flavorfusion/src/app/app.module.ts
+++ flavorfusion/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CommunityComponent } from './community/community.component';
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
@@ -16,7 +20,11 @@ import { FooterComponent } from './footer/footer.component';
     SignUpComponent,
     LoginComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    CommunityComponent,
+    SearchComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
