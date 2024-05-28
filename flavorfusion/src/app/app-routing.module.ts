62c0152447ef85088b23c1b4677b240d7c1976b1
ChrystineNoelle <asebuquechrystine@gmail.com>
2024-05-28 20:11:10 +0800

    connect header navigation to respective components

--- flavorfusion/src/app/app-routing.module.ts
+++ flavorfusion/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CommunityComponent } from './community/community.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'community', component: CommunityComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- flavorfusion/src/app/header/header.component.ts
+++ flavorfusion/src/app/header/header.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  showSearchComponent: boolean = false;
+  searchText: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (!this.router.url.includes('/search')) {
+          this.resetSearch();
+        }
+      }
+    });
+  }
+
+  toggleSearchComponent(): void {
+    this.showSearchComponent = !this.showSearchComponent;
+    if (this.showSearchComponent) {
+      this.router.navigateByUrl('/search');
+    } else {
+      this.router.navigateByUrl('/home');
+    }
+  }
+
+  closeSearch(): void {
+    this.resetSearch();
+    this.router.navigateByUrl('/home');
+  }
+
+  private resetSearch(): void {
+    this.searchText = ''; // Reset search text
+    this.showSearchComponent = false; // Hide search component
+  }
 }
