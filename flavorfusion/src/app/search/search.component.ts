a91a0077547def7bdf7e0e2c01174590b1c57c59
Via Rasquero <viarasquero20@gmail.com>
2024-05-31 20:19:33 +0800

    add search module frontend

--- flavorfusion/src/app/search/search.component.ts
+++ flavorfusion/src/app/search/search.component.ts
@@ -3,8 +3,77 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
 
+export class SearchComponent {
+  modalText: string = '';
+  inputFields: any[] = []; 
+  selectedCategories: {title: string, items: string[]}[] = [];
+  showMealTypes: boolean = false;
+  showDietaryPreferences: boolean = false;
+  showIngredients: boolean = false;
+  activeCategory: string = '';
+  activeSubCategory: string = '';
+
+  ngOnInit() {
+    window.addEventListener('click', (event: Event) => {
+      const modal = document.getElementById('my_modal') as HTMLElement;
+      if (event.target === modal) {
+        this.closeModal();
+      }
+    });
+  }
+
+  openModal(category: string) {
+    this.modalText = `Selected Category: ${category}`;
+    const modal = document.getElementById('my_modal') as HTMLElement;
+    modal.style.display = 'block';
+  }
+
+  closeModal() {
+    const modal = document.getElementById('my_modal') as HTMLElement;
+    modal.style.display = 'none';
+  }
+
+  openIngredientModal() {
+    this.modalText = 'Share the ingredients that you have:';
+    const modal = document.getElementById('my_modal') as HTMLElement;
+    modal.style.display = 'block';
+  }
+
+  searchRecipes() {
+    console.log('Searching recipes...');
+  }
+
+  addInputField() {
+    this.inputFields.push('');
+    event.preventDefault();
+  }
+
+  toggleCategory(category: string) {
+    this.showMealTypes = category === 'MealTypes' ? !this.showMealTypes : false;
+    this.showDietaryPreferences = category === 'DietaryPreferences' ? !this.showDietaryPreferences : false;
+    this.showIngredients = category === 'Ingredients' ? !this.showIngredients : false;
+  }
+
+  addCategory(categoryType: string, category: string) {
+    let categoryGroup = this.selectedCategories.find(group => group.title === categoryType);
+    if (!categoryGroup) {
+      categoryGroup = { title: categoryType, items: [] };
+      this.selectedCategories.push(categoryGroup);
+    }
+    if (!categoryGroup.items.includes(category)) {
+      categoryGroup.items.push(category);
+    }
+  }
+
+  setActiveCategory(category: string, subCategory: string) {
+    this.activeCategory = category;
+    this.activeSubCategory = subCategory;
+  }
+
+  isCategoryActive(category: string, subCategory: string): boolean {
+    return this.activeCategory === category && this.activeSubCategory === subCategory;
+  }
 }
