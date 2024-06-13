cd20a7148edd2e156feecefa7fe31c973b191db3
ManzanasMark <markangelommanzanas@iskolarngbayan.pup.edu.ph>
2024-06-13 14:05:53 +0800

    update login/logout button to be dependent on login status

--- flavorfusion/src/app/header/header.component.ts
+++ flavorfusion/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, viewChild } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 import { LoginAuthentication } from '../../services/login-authentication.service';
 
@@ -14,14 +14,20 @@ export class HeaderComponent implements OnInit {
   showSearchComponent: boolean = false;
   searchText: string = '';
 
-  constructor(private router: Router,
-              private loginAuthService: LoginAuthentication) {}
+  isLoggedIn$: Observable<boolean>;
+  loginLogoutText: string = 'Log In';
+  isShown: boolean = false;
 
+  constructor(private router: Router, 
+              private loginAuthService: LoginAuthentication) {
+    this.isLoggedIn$ = this.loginAuthService.isLoggedIn$;
+  }
   ngOnInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd && !this.router.url.includes('/search')) {
         this.resetSearch();
       }
+    this.getLoginLogoutText();
     });
   }
 
@@ -46,4 +52,42 @@ export class HeaderComponent implements OnInit {
       map((isLoggedIn: boolean) => (isLoggedIn ? '/profile': '/login'))
     );
   }
-}
+
+  @ViewChild('logInLogOut', { static: false }) logInLogOut: ElementRef;
+  getLoginLogoutText() {
+    this.loginAuthService.isLoggedIn$.subscribe((isLoggedIn: boolean) => {
+      this.logInLogOut.nativeElement.innerHTML 
+          = isLoggedIn ? 'Log Out' : 'Log In';
+    })
+  }
+
+  // TODO: Configure log in status to work independently with each user account
+  handleLoginLogout(): void {
+    this.isLoggedIn$.pipe(take(1)).subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.showModal();
+        this.loginAuthService.setIsLoggedIn(false);
+
+        const currentRoute = this.router.url;
+        if (currentRoute.includes('/profile')) {
+          this.router.navigate(['/home']);
+        }
+      } 
+      else {
+        this.router.navigate(['/login']);
+      }
+    });
+ }  
+
+  showModal() {
+    this.isLoggedIn$.subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.isShown = true;
+        timer(1000).subscribe(() => {
+          this.isShown = false;
+        });
+      }
+    });
+  }
+
+}// close class HeaderComponent
