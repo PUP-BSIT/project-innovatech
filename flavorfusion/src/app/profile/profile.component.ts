5d13dfc0dd5f43fb85d003ec401d3524d81e1d75
malaluankylarica <146600161+malaluankylarica@users.noreply.github.com>
2024-06-15 03:14:28 +0800

    add recipe functionality

--- flavorfusion/src/app/profile/profile.component.ts
+++ flavorfusion/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { RecipeService } from '../../services/recipe-service';
 import { UserService } from '../../services/user-service';
 
 @Component({
@@ -8,6 +9,7 @@ import { UserService } from '../../services/user-service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
   showSavedRecipes: boolean = false;
   showMealPlanning: boolean = false;
@@ -18,26 +20,21 @@ export class ProfileComponent implements OnInit {
   selectedFile: File | null = null;
 
   recipeForm: FormGroup;
+  // imageUrl: string | ArrayBuffer | null = null;
+  // imageUrl: string | ArrayBuffer = '';
   userProfile: any = {};
 
-  // user = {
-  //   name: 'Jane Dee',
-  //   description: 'Hello fellow food enthusiasts! I\'m Jane, \
-  //   a dedicated foodie and a weekend chef, \
-  //   always on the hunt for exciting new flavors and unique recipes.',    
-  //   password: ''
-  // };
-
   constructor(
-    private route: ActivatedRoute,
+    private route: ActivatedRoute, 
     private fb: FormBuilder, 
+    private recipeService: RecipeService,
     private userService: UserService
-  ){}
+  ) {
+    this.recipeForm = this.createRecipeForm();
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getUserProfile();
-    this.initRecipeForm();
-    
     this.route.queryParams.subscribe(params => {
       if (params['showSavedRecipes']) {
         this.showSavedRecipes = true;
@@ -45,6 +42,58 @@ export class ProfileComponent implements OnInit {
         this.showActivityLog = false;
       }
     });
+  }
+
+  private createRecipeForm(): FormGroup {
+    return this.fb.group({
+      title: ['', Validators.required],
+      description: [''],
+      mealType: ['', Validators.required],
+      dietaryPreferences: ['', Validators.required],
+      hours: [null, Validators.required],
+      minutes: [null, Validators.required],
+      servings: ['', Validators.required],
+      ingredients: this.fb.array([this.createIngredient()]),
+      instructions: this.fb.array([this.createInstruction()]),
+      // image: [null, Validators.required]
+    });
+  }
+
+  private createIngredient(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      quantity: [null, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  private createInstruction(): FormGroup {
+    return this.fb.group({
+      step: ['', Validators.required]
+    });
+  }
+
+  get ingredients(): FormArray {
+    return this.recipeForm.get('ingredients') as FormArray;
+  }
+
+  get instructions(): FormArray {
+    return this.recipeForm.get('instructions') as FormArray;
+  }
+
+  addIngredient(): void {
+    this.ingredients.push(this.createIngredient());
+  }
+
+  removeIngredient(index: number): void {
+    this.ingredients.removeAt(index);
+  }
+
+  addInstruction(): void {
+    this.instructions.push(this.createInstruction());
+  }
+
+  removeInstruction(index: number): void {
+    this.instructions.removeAt(index);
   }
 
   getUserProfile(): void {
@@ -59,100 +108,118 @@ export class ProfileComponent implements OnInit {
     });
   }
 
-  openShareRecipeModal() {
+  // handleRouteParams(): void {
+  //   this.route.queryParams.subscribe(params => {
+  //     if (params['showSavedRecipes']) {
+  //       this.showSavedRecipes = true;
+  //       this.showMealPlanning = false;
+  //       this.showActivityLog = false;
+  //     }
+  //   });
+  // }
+
+//   onImageSelected(event: Event): void {
+//     const inputElement = event.target as HTMLInputElement;
+//     if (inputElement.files && inputElement.files[0]) {
+//       const file = inputElement.files[0];
+//       this.recipeForm.patchValue({ image: file });
+//       // this.recipeForm.get('image').updateValueAndValidity();
+
+//       // Preview image
+//       const reader = new FileReader();
+//       reader.onload = () => {
+//         this.imageUrl = reader.result as string;
+//       };
+//       reader.readAsDataURL(file);
+//     }
+// }
+
+  onSubmit(): void {
+    if (this.recipeForm.valid) {
+      console.log("Form is valid, submitting...");
+      const formData = this.createFormData(this.recipeForm.value);
+
+      this.recipeService.addRecipe(formData).subscribe({
+        next: (response: any) => {
+          console.log(response);
+          if (response.success) {
+            this.recipeForm.reset();
+            this.closeShareRecipeModal();
+          } else {
+            console.error('Error adding recipe:', response);
+            alert('Error adding recipe. Please try again later.');
+          }
+        },
+        error: (error: any) => {
+          console.error('Error adding recipe:', error);
+          alert('Error adding recipe. Please try again later.');
+        }
+      });
+    }
+  }
+
+  private createFormData(formValue: any): FormData {
+    const formData = new FormData();
+    Object.keys(formValue).forEach(key => {
+      if (key === 'ingredients' || key === 'instructions') {
+        formData.append(key, JSON.stringify(formValue[key]));
+      } else if (key === 'image' && formValue[key]) {
+        formData.append('image', formValue[key], formValue[key].name);
+      } else {
+        formData.append(key, formValue[key]);
+      }
+    });
+    return formData;
+  }
+
+  openShareRecipeModal(): void {
     this.showShareRecipeModal = true;
   }
 
-  closeShareRecipeModal() {
+  closeShareRecipeModal(): void {
     this.showShareRecipeModal = false;
   }
 
-  initRecipeForm(): void {
-    this.recipeForm = this.fb.group({
-      title: ['', Validators.required],
-      description: [''],
-      mealType: ['', Validators.required],
-      dietaryPreferences: ['', Validators.required],
-      hours: [null, Validators.required],
-      minutes: [null, Validators.required],
-      servings: ['', Validators.required],
-      ingredients: this.fb.array([]),
-      instructions: this.fb.array([]),
-      image: [null]
-    });
-    this.addIngredient();
-    this.addInstruction();
-  }
-
-  get ingredients(): FormArray {
-    return this.recipeForm.get('ingredients') as FormArray;
-  }
-
-  get instructions(): FormArray {
-    return this.recipeForm.get('instructions') as FormArray;
-  }
-
-  addIngredient(): void {
-    this.ingredients.push(this.fb.group({
-      name: ['', Validators.required],
-      quantity: [null, [Validators.required, Validators.min(0)]]
-    }));
-  }
-
-  removeIngredient(index: number): void {
-    this.ingredients.removeAt(index);
-  }
-
-  addInstruction(): void {
-    this.instructions.push(this.fb.group({
-      step: ['', Validators.required]
-    }));
-  }
-
-  removeInstruction(index: number): void {
-    this.instructions.removeAt(index);
-  }
-
-  toggleSavedRecipes() {
+  toggleSavedRecipes(): void {
     this.showSavedRecipes = !this.showSavedRecipes;
     this.showMealPlanning = false;
     this.showActivityLog = false;
   }
 
-  toggleMealPlanning() {
+  toggleMealPlanning(): void {
     this.showMealPlanning = !this.showMealPlanning;
     this.showSavedRecipes = false;
     this.showActivityLog = false;
   }
 
-  toggleActivityLog() {
+  toggleActivityLog(): void {
     this.showActivityLog = !this.showActivityLog;
     this.showSavedRecipes = false;
     this.showMealPlanning = false;
   }
 
-  openEditModal() {
+  openEditModal(): void {
     this.showEditModal = true;
   }
 
-  closeEditModal() {
+  closeEditModal(): void {
     this.showEditModal = false;
   }
 
-  saveChanges() {
+  saveChanges(): void {
     console.log('Saving changes:', this.userProfile);
     this.closeEditModal();
   }
 
-  changeAvatar() {
+  changeAvatar(): void {
     this.showAvatarModal = true;
   }
 
-  closeAvatarModal() {
+  closeAvatarModal(): void {
     this.showAvatarModal = false;
   }
 
-  onFileSelected(event: any) {
+  onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
   }
 
