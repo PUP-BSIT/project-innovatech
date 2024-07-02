f96bd4684addd05067259445c1a15e36281cefd8
ManzanasMark <markangelommanzanas@iskolarngbayan.pup.edu.ph>
2024-07-02 12:24:29 +0800

    update header component, display user profile when logged in

--- flavorfusion/src/services/user-service.service.ts
+++ flavorfusion/src/services/user-service.service.ts
@@ -1,30 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from '../environments/environment';
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class UserService {
   private apiUrl = environment.apiUrl;
+  private userProfileSubject = new BehaviorSubject<any>(null);
+  userProfile$ = this.userProfileSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUserProfile(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/get_user_profile.php`,
-       { withCredentials: true });
+    return this.http.get<any>(`${this.apiUrl}/get_user_profile.php`, 
+          { withCredentials: true })
+      .pipe(tap(profile => this.userProfileSubject.next(profile)));
   }
 
   updateUserProfile(profile: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/update_user_profile.php`, 
-      profile, { withCredentials: true });
+          profile, { withCredentials: true })
+      .pipe(tap(() => this.fetchUserProfile()));
   }
 
   updateUserAvatar(formData: FormData): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/update_user_avatar.php`, 
-      formData, { withCredentials: true });
+          formData, { withCredentials: true })
+      .pipe(tap(() => this.fetchUserProfile()));
   }
 
+  private fetchUserProfile() {
+    this.getUserProfile().subscribe();
+  }
 }
-
-
