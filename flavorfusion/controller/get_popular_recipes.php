0d4aef0b5f85ec956f38fdc4a73c915dbdf04fcd
ChrystineNoelle <asebuquechrystine@gmail.com>
2024-07-06 02:23:16 +0800

    update get_popular_recipes.php

--- flavorfusion/controller/get_popular_recipes.php
+++ flavorfusion/controller/get_popular_recipes.php
@@ -5,12 +5,16 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 require_once 'db_connection.php';
 
-$sql = "SELECT r.recipe_id, r.name, r.picture
+$sql = "SELECT r.recipe_id, r.name, r.picture,
+               COUNT(sr.saved_recipe_id) AS save_count,
+               COUNT(rt.rating_id) AS rating_count,
+               (COUNT(sr.saved_recipe_id) + COUNT(rt.rating_id)) AS popularity_score
         FROM recipes r
         LEFT JOIN saved_recipes sr ON r.recipe_id = sr.recipe_id
         LEFT JOIN ratings rt ON r.recipe_id = rt.recipe_id
         GROUP BY r.recipe_id
-        ORDER BY COUNT(sr.saved_recipe_id) + COUNT(rt.rating_id) DESC
+        HAVING save_count > 0 OR rating_count > 0
+        ORDER BY popularity_score DESC
         LIMIT 8";
 
 $result = $conn->query($sql);
